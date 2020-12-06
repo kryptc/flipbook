f12677afec53e0d77e22a437f66d6bbd808f7e35
kryptc <libra.anne.99@gmail.com>
2020-12-06 20:34:26 +0530

    added multiple(2) images

--- main.py
+++ main.py
@@ -13,21 +13,36 @@ class FlipBook :
     
     def __init__(self):
         self.frame_list = []
-        self.blank = np.zeros([720,720,3],dtype=np.uint8)
+        self.dim = 720
+        self.blank = np.zeros([self.dim,self.dim,3],dtype=np.uint8)
         self.blank.fill(0)
         
     def generate_gif(self, frames):
         for frame in frames:
-            #if len(frame) == 3 // standard appending
             startFrame = frame[0]
             endFrame = frame[1]
-            imageName = frame[2]
+            print (frame, len(frame))
+            if len(frame) == 3: # standard appending
+                imageName = frame[2]
+            else:
+                imageName = self.combine_images(frame[2:])
             self.add_image_to_frame_list(startFrame, endFrame, imageName)
 
         imageio.mimsave('flipbook.gif', self.frame_list)
         print("GIF named flipbook.gif has been generated")
         
-    
+    def combine_images(self, imageList):
+        #assume 2 images being horizontally concatenated
+        n = len(imageList)
+        im1 = Image.open(imageList[0])
+        im2 = Image.open(imageList[1])
+        # dst = Image.new('RGB', (im1.width + im2.width, min(im1.height, im2.height)))
+        dst = Image.new('RGB', (self.dim, self.dim))
+        dst.paste(im1, (0, 0))
+        # dst.paste(im2, (im1.width, (im1.height - im2.height) // 2))
+        dst.paste(im2, (im1.width, 0))
+        return dst
+
     def add_image_to_frame_list(self,startFrame, endFrame, imageName):
         """ add other params/functions to do resizing/positioning etc """       
         for i in range(startFrame-1, endFrame-1):
@@ -37,7 +52,7 @@ class FlipBook :
                 im = im.resize((720, 720))
                 self.frame_list.append(im)
                 self.frame_list.append(im)
-                
+
             except:
                 print (imageName, " not found.")
                 # BufferedImage bi= new BufferedImage(320,240,BufferedImage.TYPE_BYTE_GRAY);
--- parser.py
+++ parser.py
@@ -22,6 +22,28 @@ def parser(text):
 			else:
 				print ("Syntax error on line " + str(it))
 				return [0,0]
+		#combine command, doing only for 2 pics
+		elif len(tokens) == 6:
+			if tokens[0].isdigit() and tokens[1].isdigit() and tokens[2] == "combine" and tokens[3].isdigit():
+				
+				if int(tokens[0]) > int(tokens[1]):
+					print ("Syntax error on line " + str(it) + ": starting frame should be lesser than or equal to ending frame.")
+					return [0,0]
+
+				framearr = []
+				framearr.append(int(tokens[0]))
+				framearr.append(int(tokens[1]))
+				for i in range(1, int(tokens[3])+1):
+					if tokens[3+i].endswith(extensions):
+						framearr.append(tokens[3+i])
+					else:
+						print ("Syntax error on line " + str(it) + ": pls use images with the extensions " + extensions)
+						return [0,0]
+				frame_img.append(framearr)
+				continue
+			else:
+				print ("Syntax error on line " + str(it))
+				return [0,0]
 		else:
 			print ("Syntax error on line " + str(it) + ": too many or too few arguments in a single line")
 			return [0,0]
